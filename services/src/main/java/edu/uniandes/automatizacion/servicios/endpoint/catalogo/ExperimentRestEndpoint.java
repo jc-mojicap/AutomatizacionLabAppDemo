4bdc0ae4a7b5fd943880ae7b6bd57afb9f454b21
Gcruz <g.cruz@uniandes.edu.co>
2018-10-21 22:14:44 -0500

    endpoint

---
+++ services/src/main/java/edu/uniandes/automatizacion/servicios/endpoint/catalogo/ExperimentRestEndpoint.java
@@ -0,0 +1,30 @@
+package edu.uniandes.automatizacion.servicios.endpoint.catalogo;
+
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.uniandes.automatizacion.servicios.dao.model.Experiment;
+import edu.uniandes.automatizacion.servicios.service.catalogo.iface.ExperimentService;
+
+@RestController
+public class ExperimentRestEndpoint {
+	
+	private static final String MODULO = "catalogo";
+    
+    private static final String ENTIDAD = "experiment";
+
+    @Autowired
+    private ExperimentService experimentService;
+   
+    
+    @RequestMapping(value = "/" + MODULO + "/" + ENTIDAD, method = RequestMethod.GET)
+    public Experiment getExperiment() throws URISyntaxException {
+    	return experimentService.initializeExperiment();
+    }
+    
+    
+}
